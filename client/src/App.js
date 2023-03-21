9f6f444dec1b96b46281ac1a892a908042e4478b
Munyaradzi <syntaxread3r@gmail.com>
2023-03-21 19:38:53 +0000

    add styles for Login.jsx and some logic

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Write from "./pages/Write";
 
 import Layout from "./components/Layout";
 
+import './styles/style.scss'
+
 // Define routing configuration using createBrowserRouter with relevant paths and associated components.
 const router = createBrowserRouter([
 	{

9c1eb3519cdd96189d254fbbe2262428488e2d9e
Munyaradzi <syntaxread3r@gmail.com>
2023-03-23 22:48:51 +0000

    push a functional local connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import postRoutes from './routes/posts.js'
 
 const app = express();
 
 app.use(express.json());
+app.use('/api/posts', postRoutes)
+
 
 app.listen(9000, () => {
 	console.log("Connected");

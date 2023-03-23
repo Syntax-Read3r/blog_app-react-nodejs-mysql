a996715cf668bc1a937bdd5f8a67a9422a4eb199
Munyaradzi <syntaxread3r@gmail.com>
2023-03-23 21:38:04 +0000

    emptied server folder-- huge bug found

--- server/routes/testAPI.js
+++
@@ -1,8 +0,0 @@
-var Express = require("express");
-var router = Express.Router();
-
-router.get("/", function (req, res, next) {
-	res.send("API is working properly");
-});
-
-module.exports = router;

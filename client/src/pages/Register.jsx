787c1c534a9bfa256ad5ff00bb0644f9ab79dd60
Munyaradzi <syntaxread3r@gmail.com>
2023-03-24 11:48:24 +0000

    add logic to Client > Pages > Register.jsx to handle inputs using useState and write func handleChange and handleSubmit

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,59 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+	const [inputs, setInputs] = useState({
+		username: "",
+		email: "",
+		password: "",
+	});
+
+	const handleChange = (e) => {
+		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const handleSubmit = async (e) => {
+		// Since this is an API request. An async function is required on this instance and axios library
+		e.preventDefault();
+		try {
+			const res = await axios.post("/auth/register", inputs);
+			console.log(res);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
 		<div className="auth">
 			<h1>Register</h1>
 			<form action="">
-				<input required type="text" name="" id="" placeholder="username" />
-				<input required type="email" name="" id="" placeholder="email" />
-				<input required type="password" name="" id="" placeholder="password" />
-				<button>Register</button>
+				<input
+					required
+					type="text"
+					name="username"
+					id=""
+					placeholder="username"
+					onChange={handleChange}
+				/>
+				<input
+					required
+					type="email"
+					name="email"
+					id=""
+					placeholder="email"
+					onChange={handleChange}
+				/>
+				<input
+					required
+					type="password"
+					name="password"
+					id=""
+					placeholder="password"
+					onChange={handleChange}
+				/>
+				<button onClick={handleSubmit}>Register</button>
 				<p>this is an error!</p>
 				<span>
 					Do you have an account? <Link to="/login">Login</Link>

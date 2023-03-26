5cae0ac05c6b96c2e1ef890cc473c1f67d98c04d
Munyaradzi <syntaxread3r@gmail.com>
2023-03-26 16:52:50 +0100

    add a refractored auth.js > register function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,26 +1,37 @@
 import { db } from "../database/db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const jwtkey = process.env.JWT_PRIVATE_KEY
 
 // NOTE: REGISTER
 
 export const register = (req, res) => {
 	//CHECK EXISTING USER
-	const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+	const selectQuery =
+		"SELECT COUNT(*) AS count FROM users WHERE email = ? OR username = ?";
+	const insertQuery =
+		"INSERT INTO users(`username`,`email`,`password`) VALUES (?,?,?)";
 
-	db.query(q, [req.body.email, req.body.username], (err, data) => {
+	db.query(selectQuery, [req.body.email, req.body.username], (err, data) => {
 		if (err) return res.status(500).json(err);
-		if (data.length) return res.status(409).json("User already exists!");
+		if (data[0].count) return res.status(409).json("User already exists!");
 
 		//Hash the password and create a user
 		const salt = bcrypt.genSaltSync(10);
-		const hash = bcrypt.hashSync(req.body.password, salt);
-
-		const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+		let hash;
+		try {
+			hash = bcrypt.hashSync(req.body.password, salt);
+		} catch (error) {
+			console.error("Error occurred while hashing password:", error);
+			return res.status(500).json({ error: error.message });
+		}
 		const values = [req.body.username, req.body.email, hash];
 
-		db.query(q, [values], (err, data) => {
+		db.query(insertQuery, values, (err, data) => {
 			if (err) return res.status(500).json(err);
 			return res.status(200).json("User has been created.");
 		});
@@ -45,7 +56,7 @@ export const login = (req, res) => {
 		if (!isPasswordCorrect)
 			return res.status(400).json("Wrong username or password!");
 
-		const token = jwt.sign({ id: data[0].id }, "jwtkey");
+		const token = jwt.sign({ id: data[0].id }, jwtkey);
 		const { password, ...other } = data[0];
 
 		res
--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,12 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"",
-  password: '',
-  database:"blog_app"
-})
+	host: "localhost",
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_KEY,
+	database: "blog_app",
+});
